9fa23021a4b3f951d108080a7226f7986527245d
Mykhailo Gerasymenko <mihail@gerasimenko.me>
2021-08-31 17:22:31 +0200

    Initial Commit

---
+++ Shared/UndoRedoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UndoRedoAppApp.swift
+//  Shared
+//
+//  Created by Mike on 31.08.21.
+//
+
+import SwiftUI
+
+@main
+struct UndoRedoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
