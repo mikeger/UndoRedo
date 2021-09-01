f2012e9dee4ffab75f34a61bfd99e8864b993d8c
Mykhailo Gerasymenko <mihail@gerasimenko.me>
2021-09-01 10:28:39 +0200

    Added: Initial version with Combine-based Undo/Redo

---
+++ CommandManagerTests/CommandManagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  CommandManagerTests.swift
+//  CommandManagerTests
+//
+//  Created by Mike on 01.09.21.
+//
+
+import XCTest
+
+class CommandManagerTests: XCTestCase {
+
+    func testExample() throws {
+        
+        
+    }
+
+}
